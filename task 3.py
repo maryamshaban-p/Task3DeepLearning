46a007cc9511f14f43b393a06862626c12f1c71a
Maryam Shaban <mariem.ahmed22377@ci.menofia.edu.eg>
2025-12-07 05:26:57 +0200

    Update task 3.py

--- task 3.py
+++ task 3.py
@@ -1,37 +1,64 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torchtext.datasets import IMDB
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+from torch.nn.utils.rnn import pad_sequence
 
-input_size = 5
-hidden_size = 10
+tokenizer = get_tokenizer('basic_english')
+train_iter = IMDB(split='train')
+
+def yield_tokens(data_iter):
+    for _, text in data_iter:
+        yield tokenizer(text)
+
+vocab = build_vocab_from_iterator(yield_tokens(train_iter), specials=["<unk>"])
+vocab.set_default_index(vocab["<unk>"])
+
+def process(text):
+    return torch.tensor(vocab(tokenizer(text)), dtype=torch.long)
+
+train_iter = IMDB(split='train')
+data = []
+for label, text in train_iter:
+    data.append((0 if label == "neg" else 1, process(text)))
+
+input_size = len(vocab)
+hidden_size = 64
 output_size = 1
-seq_length = 4
-batch_size = 2
+embedding_dim = 64
+batch_size = 8
 
-x = torch.randn(seq_length, batch_size, input_size)
-y = torch.randn(batch_size, output_size)
-
-class SimpleRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(SimpleRNN, self).__init__()
-        self.rnn = nn.RNN(input_size, hidden_size, batch_first=False)
+class LSTMModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embedding = nn.Embedding(input_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_size)
         self.fc = nn.Linear(hidden_size, output_size)
+        self.sig = nn.Sigmoid()
 
     def forward(self, x):
-        out, hidden = self.rnn(x)
-        last_out = out[-1]
-        out = self.fc(last_out)
-        return out
+        x = self.embedding(x)
+        out, (h, c) = self.lstm(x)
+        return self.sig(self.fc(out[-1]))
 
-model = SimpleRNN(input_size, hidden_size, output_size)
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+model = LSTMModel()
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-optimizer.zero_grad()
-output = model(x)
-loss = criterion(output, y)
-loss.backward()
-optimizer.step()
+for epoch in range(1):
+    total_loss = 0
+    for i in range(0, len(data), batch_size):
+        batch = data[i:i+batch_size]
+        labels = torch.tensor([x[0] for x in batch], dtype=torch.float32).unsqueeze(1)
+        texts = [x[1] for x in batch]
+        texts = pad_sequence(texts).to(labels.device)
 
-print("Output:", output)
-print("Loss:", loss.item())
+        optimizer.zero_grad()
+        preds = model(texts)
+        loss = criterion(preds, labels)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    print("Epoch Loss:", total_loss)
